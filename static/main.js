cc391be47fc17dca9c151fc4fbeb28971136c9e0
Eric Liu <ericliu404@gmail.com>
2018-07-13 15:00:26 -0400

    New: Add score page navigation and player controls
    This updates dev to the latest work done on gh-pages branch which was somewhat bootstrap reliant

--- static/main.js
+++ static/main.js
@@ -1,9 +1,11 @@
-// Manifest stuff
+// Manifest fetching and callback actions
 var manifestObject; 
-$('#getURL').click(function () {
+$('#getURL').click(function () 
+{
     let url = $('#urlBox').val();
     manifestObject = new ManifestObject(url); 
-    manifestObject.fetchManifest(function () {
+    manifestObject.fetchManifest(function () 
+    {
         // callback once manifest is fetched
         // set title
         let title = manifestObject.manifest.item_title;
@@ -14,29 +16,56 @@ $('#getURL').click(function () {
         // link score
         renderVerovio();
         trackVideo();
+
+        $("#player_controls").show();
+        $("#score_controls").show();
     });
 });
 
-// Verovio stuff
+
+// Verovio score rendering and score manipulation
 var toolkit = new verovio.toolkit();
-async function renderVerovio () {
+var page = 0; 
+async function renderVerovio () 
+{
     await $.ajax({
         url: "static/mei/demo.mei", 
         dataType: "text", 
-        success: function(data) {
-            var mei = toolkit.renderData(data, {});
-            for (var i = 1; i <= toolkit.getPageCount(); i++) 
+        success: function (data) 
+        {
+            $('.score').empty(); // clear previous verovio renderings
+            toolkit.renderData(data, {});
+            for (var i = 1; i <= toolkit.getPageCount(); i++) // verovio pages are 1-indexed
             {
                 let svg = toolkit.renderPage(i, {});
-                $(".score").append(svg);
+                $('.score').append(svg);
             }
+            $('.score').children().not($('.score').children().first()).hide(); // hide other pages
             $('svg').width("100%");
             $('svg').height("100%");
         }
     });
     linkScore();
 };
+function nextPage ()
+{
+    if (page === toolkit.getPageCount()-1)
+        return;
+    $('.score').children().eq(page).hide();
+    page++;
+    $('.score').children().eq(page).show();
+}
+function prevPage ()
+{
+    if (page === 0)
+        return;
+    $('.score').children().eq(page).hide();
+    page--;
+    $('.score').children().eq(page).show();
+}
 
+
+// score and player syncing
 function linkScore ()
 {
     let increment = manifestObject.manifest.canvases[0].duration / $('.measure').length;
@@ -53,23 +82,69 @@ function linkScore ()
         $('video')[0].play();
     });
 };
-
-// track video progress
+// track video progress and move score highlight
 function trackVideo ()
 {
     setInterval(function () 
     {
         let time = $('video')[0].currentTime;
-        $('.measure').each(function () {
-            if (time > $(this).attr('time')) {
-                fillMeasure(this);
-            }
-        });
+        if (time != 0) {
+            $('.measure').each(function () {
+                if (time >= $(this).attr('time')) {
+                    fillMeasure(this);
+                }
+            });
+        }
     }, 300);
 }
-
 function fillMeasure (measure) 
 {
     $(measure).attr('fill', '#d00');
     $('.measure').not(measure).removeAttr('fill');
+}
+
+
+// video and score control 
+function buttonPlayPress()
+{
+	if ($('video')[0].paused) {
+		$('video')[0].play();
+		d3.select("#button_play i").attr('class', "fa fa-pause");    
+	} else {
+		$('video')[0].pause();
+		d3.select("#button_play i").attr('class', "fa fa-play");
+	}
+}
+function buttonStopPress()
+{
+	d3.select("#button_play i").attr('class', "fa fa-play");
+
+	$('video')[0].pause();
+	$('video')[0].currentTime = 0;
+
+	$('.measure').removeAttr('fill');
+}
+function buttonBackPress()
+{
+	let measureFound = false;
+	let time = $('video')[0].currentTime;
+    $($('.measure').get().reverse()).each(function () {
+        if ($(this).attr('time') < time) {
+        	if (measureFound) {
+        		$('video')[0].currentTime = $(this).attr('time');
+        		return false;
+        	}
+        	measureFound = true;
+        }
+    });
+}
+function buttonForwardPress()
+{
+    let time = $('video')[0].currentTime;
+    $('.measure').each(function () {
+        if ($(this).attr('time') > time) {
+            $('video')[0].currentTime = $(this).attr('time');
+            return false;
+        }
+    });
 }
