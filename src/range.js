4b7559aef34a5eb1dacec008c2ffdc17104949a3
Andrew Kam <andrew.kam@outlook.com>
2018-08-22 15:42:50 -0400

    New: Add progress bar identifiers

--- src/range.js
+++ src/range.js
@@ -16,7 +16,7 @@ export class Range
         let row = $('<div class="row" style="padding-bottom:5px"></div>');
         let column = $('<div class="col-md-6 offset-md-3"></div>');
         let progress = $('<div class="progress" style="height:8px"></div>');
-        let progressBar = $('<div class="progress-bar bg-info w-0" role="progressbar" style="transition:none"></div>');
+        let progressBar = $('<div class="progress-bar bg-info w-0" id="' + this.label + '" role="progressbar" style="transition:none"></div>');
 
         progress.append(progressBar);
         column.append(progress);
